28a072ec1f76a2343b4bda7dd3834feff289b255
Victor Ramos <v.ramos58@hotmail.com>
2025-10-01 17:37:20 -0300

    convertando apenas quando não passado nenhum valor

--- desafio/main.go
+++ desafio/main.go
@@ -1,20 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
 	println("Digite a temperatura em Celsius:")
 
-	var celsius float64
+	var celsius string
 	fmt.Scanln(&celsius)
 
 	var kelvin float64
 
-	if celsius == 0 {
+	if celsius == "" {
 		kelvin = 100 + 273.15
-		fmt.Println("A de ebulição em Kelvin é:", kelvin)
+		fmt.Println("A temperatura de ebulição em Kelvin é:", kelvin)
 	} else {
-		kelvin = celsius + 273.15
-		fmt.Println("A temperatura em Kelvin é:", kelvin)
+		temp, err := strconv.ParseFloat(celsius, 64)
+		if err != nil {
+			fmt.Println("Erro: valor inválido. Usando temperatura de ebulição.")
+			kelvin = 100 + 273.15
+			fmt.Println("A temperatura de ebulição em Kelvin é:", kelvin)
+		} else {
+			kelvin = temp + 273.15
+			fmt.Printf("A temperatura %.2f°C em Kelvin é: %.2f K\n", temp, kelvin)
+		}
 	}
 }
